6f0d92dcff728c032590c8230cee7650ca1cf78d
lc <liuchao@xm12t.com>
2016-04-27 14:14:30 +0800

    quickbar外链支持换名字-bug修复

--- js/quickbar-p1.js
+++ js/quickbar-p1.js
@@ -113,7 +113,7 @@ function jqueryfunc() {
                         li_class = "quickbar_map";
                         li_target = 'target="_blank"';
                     } else if (v.type == 'link') { //外链
-                        if(v.link.indexOf('<{}>')){
+                        if(v.link.indexOf('<{}>') >= 0){
                             var linkArr = v.data.split("<{}>");
                             v.name = linkArr[0];
                             v.link = linkArr[1];
