feedc3907c8bb7d56a33dc874b710ba8a42a7e8b
lc <liuchao@xm12t.com>
2016-06-29 11:35:13 +0800

    quickbar手机和pc判断分离

--- js/quickbar.js
+++ js/quickbar.js
@@ -46,7 +46,14 @@ document.getElementById("quickbar-share") !== null ? document.getElementById("qu
 		}
 	}
 	if (target.src.indexOf('?') == -1 || target.src.substring(target.src.indexOf('?') + 1).indexOf('debug') == -1) {
-		configQuickbar.dataurl = '/quickbar.json';
+		var winLoca = window.location.href;
+		winLoca = winLoca.replace(/(^http\:\/\/*)/g, "");
+		winLoca = winLoca.split('/');
+		if(winLoca[1]=='mobile'){
+			configQuickbar.dataurl = '/mobile/quickbar.json';
+		}else{
+			configQuickbar.dataurl = '/quickbar.json';
+		}
 		if (target.src.indexOf('?') !== -1) {
 			var param = target.src.substring(target.src.indexOf('?') + 1);
 			window.CustomerID = param.match(/(\d*)(.*)/)[1];
