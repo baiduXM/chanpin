7833f78f49f48f82752d4e3b2b03a65488d91123
llyynnee <2279083016@qq.com>
2016-06-30 12:22:50 +0800

    出错修改

--- js/img.js
+++ js/img.js
@@ -4,6 +4,5 @@ jsimglarge.setAttribute("src", "http://chanpin.xm12t.com.cn/js/imglarge.js");
 var headobj = document.getElementsByTagName("head")[0];headobj.appendChild(jsimglarge);
 var imglargeStyle = document.createElement('style');
 imglargeStyle.setAttribute('type', 'text/css');
-imglargeStyle.setAttribute('rel', 'stylesheet');
-imglargeStyle.setAttribute('src', 'http://chanpin.xm12t.com.cn/css/imglarge.css');
+imglargeStyle.setAttribute('href', 'http://chanpin.xm12t.com.cn/css/imglarge.css');
 (document.head || document.getElementsByTagName('head')[0]).appendChild(imglargeStyle);
