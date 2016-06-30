4f6819ac02845208506f0524fd1905112ec9d80c
llyynnee <2279083016@qq.com>
2016-06-30 11:46:22 +0800

    修改图片放大功能

---
+++ js/img.js
@@ -0,0 +1,8 @@
+var jsimglarge = document.createElement('script');
+jsimglarge.setAttribute("type", "text/javascript");
+jsimglarge.setAttribute("src", "http://chanpin.xm12t.com.cn/js/imglarge.js");
+var headobj = document.getElementsByTagName("head")[0];headobj.appendChild(jsimglarge);
+imglargeStyle.setAttribute('type', 'text/css');
+imglargeStyle.setAttribute('rel', 'stylesheet');
+imglargeStyle.setAttribute('src', 'http://chanpin.xm12t.com.cn/css/imglarge.css');
+(document.head || document.getElementsByTagName('head')[0]).appendChild(imglargeStyle);
