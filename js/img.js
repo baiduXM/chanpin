0862b6043eadb77a2a24ac1b357107f3cd086260
llyynnee <2279083016@qq.com>
2016-06-30 11:52:12 +0800

    bug修复

--- js/img.js
+++ js/img.js
@@ -2,6 +2,7 @@ var jsimglarge = document.createElement('script');
 jsimglarge.setAttribute("type", "text/javascript");
 jsimglarge.setAttribute("src", "http://chanpin.xm12t.com.cn/js/imglarge.js");
 var headobj = document.getElementsByTagName("head")[0];headobj.appendChild(jsimglarge);
+var imglargeStyle = document.createElement('style');
 imglargeStyle.setAttribute('type', 'text/css');
 imglargeStyle.setAttribute('rel', 'stylesheet');
 imglargeStyle.setAttribute('src', 'http://chanpin.xm12t.com.cn/css/imglarge.css');
